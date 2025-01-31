6c5bcf10f5fd5be218936e842537e8afd4456965
cheesestick24 <ht17a075@oecu.jp>
2025-01-31 12:15:43 +0900

    port変更

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(express.static("public"));
@@ -77,5 +77,5 @@ app.post("/shuffle", (req, res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
